c35bbbc700c9e3c4dbd8b1d6d1c0449722ef8ead
Guilherme Muller <guilhermemullerxx@gmail.com>
2025-03-20 08:54:59 -0300

    Adiciona a dependência dotenv e atualiza a configuração do GoogleGenerativeAI para usar a chave da API a partir de variáveis de ambiente. Remove código comentado e trechos não utilizados no componente Home.

--- app/page.tsx
+++ app/page.tsx
@@ -9,22 +9,17 @@ export default function Home() {
   const [chatResponse, setChatResponse] = useState("");
   const [isThinking, setIsThinking] = useState(false);
 
-  const genAI = new GoogleGenerativeAI("AIzaSyAno6ZrbgKBpR86wpdUlF8dypM0kICPXB4")
+  const genAI = new GoogleGenerativeAI(process.env.API_KEY as string);
   const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
   const prompt = userMessage;
 
   const formatText = (text: string) => {
-    // Substituir ** por <strong> para negrito
     let formattedText = text.split('**').map((part, index) =>
       index % 2 === 1 ? `<strong>${part}</strong>` : part
     ).join('');
 
-    // Substituir quebras de linha (\n) por <br />
     formattedText = formattedText.replace(/\n/g, '<br />');
-
-    // Remover qualquer instância de [object Object] no texto
     formattedText = formattedText.replace(/\[object Object\]/g, '');
-
     return formattedText;
   };
 
@@ -40,7 +35,7 @@ export default function Home() {
     }
 
     setUserMessage("");
-    
+
     setIsThinking(true);
     setChatResponse("");
     const result = await model.generateContent(prompt);
@@ -90,42 +85,4 @@ export default function Home() {
       </div>
     </>
   )
-
-  return (
-    <>
-      <div className="h-screen w-screen flex flex-col items-center justify-between bg-gray-900 text-white p-4">
-
-        <div className="w-full max-w-2xl flex flex-col gap-4">
-          <h1 className="text-3xl font-semibold text-center">Pergunte ao Chat!</h1>
-
-          <form onSubmit={getChatResponse} className="flex gap-2">
-            <input
-              type="text"
-              placeholder="Digite sua pergunta..."
-              className="w-full border-2 border-gray-600 bg-gray-800 text-white h-12 px-5 rounded-lg text-sm focus:outline-none focus:border-blue-500"
-              value={userMessage}
-              onChange={(e) => setUserMessage(e.target.value)}
-            />
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
-              type="submit"
-              disabled={isThinking}
-            >
-              {isThinking ? "Pensando..." : "Enviar"}
-            </button>
-          </form>
-
-          {/* Container para a resposta com rolagem */}
-          {chatResponse && (
-            <div className="bg-gray-800 p-4 rounded-lg h-[75vh] overflow-y-auto">
-              <p
-                className="text-md font-normal text-gray-200"
-                dangerouslySetInnerHTML={{ __html: formatText(chatResponse) }}
-              />
-            </div>
-          )}
-        </div>
-
-      </div>
-    </>
-  );
 }
